64362bcdbd0b9c06fd69c3e7c7dfcbe14e120c01
vachanbiyani <67270111+vachanbiyani@users.noreply.github.com>
2020-08-13 21:56:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,68 +1,56 @@
-//Create variables here
-var dog,happyDog,database;
-var dogImage,happyDogImage;
-var foodStock,foodS;
-var milk =20;
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
 
-function preload()
-{
-  //load images here
-  dogImage=loadImage("dogImg.png");
-  dogImage.scale=0.05
-  happyDogImage=loadImage("dogimg1.png");
-}
-
-function setup() {
-  createCanvas(1600,800);
-  dog=createSprite(700,370);
-  dog.addImage(dogImage);
-  database=firebase.database();
-
-  foodStock=database.ref("Food");
-  foodStock.on("value",readStock);
-
-  
-}
-
-
-function draw() {  
-  background(46,139,87);
-
-  if(keyCode===UP_ARROW){
-    writeStock(foodS);
-    
-    dog.addImage(happyDogImage);
-    milk=19;
-    
-
+function preload(){
+   dogImg=loadImage("Images/Dog.png");
+   dogImg1=loadImage("Images/happy dog.png");
   }
-  
-  
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
 
   drawSprites();
-  //add styles here
-  textSize(30);
-  fill("yellow");
-  text("Note:Press UP_ARROW To Feed The Dog",10,25);
-  text("Milk Bottle ="+milk,50,100);
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
 
-  }
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
 
-  function readStock(data){
-    foodS=data.val();
-  }
-
-  function writeStock(x){
-
-    if(x<=0){
-      x=0;
-    }else{
-      x=x-1;
-    }
-    database.ref('/').update({
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
     Food:x
-    })
-  }
-
-
-
+  })
+}
